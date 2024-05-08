c1b210e57e38fd7da4e834b3016a047fdc561980
hoosa <janghoosa@gmail.com>
2024-05-08 23:15:33 +0900

    feat: redis pub 구현(#77)

---
+++ src/main/java/com/palgona/palgona/common/WebSocket/AuthChannelInterceptor.java
@@ -0,0 +1,33 @@
+package com.palgona.palgona.common.WebSocket;
+
+import com.palgona.palgona.common.jwt.util.JwtUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+@Slf4j
+public class AuthChannelInterceptor implements ChannelInterceptor {
+    private final JwtUtils jwtUtils;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        if (StompCommand.CONNECT == accessor.getCommand()) {
+            String jwtToken = accessor.getFirstNativeHeader("Authorization");
+            try {
+                jwtUtils.isExpired(jwtToken);
+            } catch (Exception e) {
+                log.info("socket jwt exception {}", e.getMessage());
+                throw e;
+            }
+        }
+        return message;
+    }
+}
--- src/main/java/com/palgona/palgona/common/entity/BaseTimeEntity.java
+++ src/main/java/com/palgona/palgona/common/entity/BaseTimeEntity.java
@@ -1,5 +1,10 @@
 package com.palgona.palgona.common.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -13,11 +18,16 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 @Getter
 public class BaseTimeEntity {
-
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm")
     @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm")
     @LastModifiedDate
     private LocalDateTime updatedAt;
 }
