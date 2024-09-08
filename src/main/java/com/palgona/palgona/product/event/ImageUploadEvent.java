398a1dd365136d05894c70e748607ba9d055ff0a
jinsu868 <mjinsu098@gmail.com>
2024-09-08 18:11:55 +0900

    feat: 프로필 이미지 수정 비동기 처리

--- src/main/java/com/palgona/palgona/product/event/ImageUploadEvent.java
+++ src/main/java/com/palgona/palgona/product/event/ImageUploadEvent.java
@@ -6,4 +6,8 @@ import java.util.List;
 public record ImageUploadEvent(
         List<ImageUploadRequest> imageUploadRequests
 ) {
+
+    public static ImageUploadEvent from(List<ImageUploadRequest> imageUploadRequests) {
+        return new ImageUploadEvent(imageUploadRequests);
+    }
 }
