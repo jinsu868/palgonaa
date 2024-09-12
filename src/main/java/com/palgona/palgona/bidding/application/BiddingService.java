27dfb64744cac46d17a04a6c048ccc739d81d862
jinsu868 <mjinsu098@gmail.com>
2024-09-12 14:29:25 +0900

    refactor: bidding 검증 로직 리팩토링

--- src/main/java/com/palgona/palgona/bidding/application/BiddingService.java
+++ src/main/java/com/palgona/palgona/bidding/application/BiddingService.java
@@ -3,16 +3,15 @@ package com.palgona.palgona.bidding.application;
 import static com.palgona.palgona.common.error.code.BiddingErrorCode.BIDDING_EXPIRED_PRODUCT;
 import static com.palgona.palgona.common.error.code.BiddingErrorCode.BIDDING_INSUFFICIENT_BID;
 import static com.palgona.palgona.common.error.code.BiddingErrorCode.BIDDING_LOWER_PRICE;
-import static com.palgona.palgona.common.error.code.MemberErrorCode.MEMBER_NOT_FOUND;
 import static com.palgona.palgona.common.error.code.ProductErrorCode.NOT_FOUND;
 
+import com.palgona.palgona.common.annotation.DistributedLock;
 import com.palgona.palgona.common.error.exception.BusinessException;
 import com.palgona.palgona.bidding.domain.Bidding;
 import com.palgona.palgona.bidding.domain.BiddingState;
 import com.palgona.palgona.member.domain.Member;
 import com.palgona.palgona.product.domain.Product;
 import com.palgona.palgona.purchase.domain.Purchase;
-import com.palgona.palgona.bidding.dto.request.BiddingAttemptRequest;
 import com.palgona.palgona.bidding.domain.BiddingRepository;
 import com.palgona.palgona.purchase.infrastructure.PurchaseRepository;
 import com.palgona.palgona.member.domain.MemberRepository;
@@ -35,47 +34,59 @@ public class BiddingService {
     private final PurchaseRepository purchaseRepository;
     private final MemberRepository memberRepository;
 
-    @Transactional
-    public void attemptBidding(Member member, BiddingAttemptRequest request) {
-        Product product = findProductWithPessimisticLock(request);
+    @DistributedLock(key = "#productId")
+    public void attemptBidding(Long productId, Member biddingMember, int attemptPrice) {
+        Product product = findProductWithPessimisticLock(productId);
 
-        int attemptPrice = request.price();
+        validateProductDeadline(product);
+        validateBiddingCreate(attemptPrice, product);
 
-        if (product.isDeadlineReached()) {
-            throw new BusinessException(BIDDING_EXPIRED_PRODUCT);
-        }
+        int previousBid = biddingRepository.findHighestPriceByMember(biddingMember).orElse(0);
+        int extraCost = attemptPrice - previousBid;
+        biddingMember.useMileage(extraCost);
+        Bidding bidding = Bidding.builder()
+                .member(biddingMember)
+                .product(product)
+                .price(attemptPrice)
+                .build();
 
+        biddingRepository.save(bidding);
+        memberRepository.save(biddingMember);
+    }
+
+    private void validateBiddingCreate(int attemptPrice, Product product) {
         int highestPrice = biddingRepository.findHighestPriceByProduct(product).orElse(0);
+        int threshold = (int) Math.pow(10, String.valueOf(attemptPrice).length() - 2);
+        int priceDifference = attemptPrice -  highestPrice;
+
+        if (attemptPrice < product.getInitialPrice()) {
+            throw new BusinessException(BIDDING_LOWER_PRICE);
+        }
 
         if (attemptPrice <= highestPrice) {
             throw new BusinessException(BIDDING_LOWER_PRICE);
         }
 
-        int threshold = (int) Math.pow(10, String.valueOf(attemptPrice).length() - 2);
-        int priceDifference = attemptPrice - highestPrice;
-
         if (priceDifference < threshold) {
             throw new BusinessException(BIDDING_INSUFFICIENT_BID);
         }
-
-        Member biddingMember = findMemberWithPessimisticLock(member);
-        int previousBid = biddingRepository.findHighestPriceByMember(biddingMember).orElse(0);
-        int extraCost = attemptPrice - previousBid;
-        biddingMember.useMileage(extraCost);
-        Bidding bidding = Bidding.builder().member(biddingMember).product(product).price(attemptPrice).build();
-
-        biddingRepository.save(bidding);
     }
 
-    private Product findProductWithPessimisticLock(BiddingAttemptRequest request) {
-        return productRepository.findByIdWithPessimisticLock(request.productId())
+    private void validateProductDeadline(Product product) {
+        if (product.isDeadlineReached()) {
+            throw new BusinessException(BIDDING_EXPIRED_PRODUCT);
+        }
+    }
+
+    private Product findProductWithPessimisticLock(Long id) {
+        return productRepository.findByIdWithPessimisticLock(id)
                 .orElseThrow(() -> new BusinessException(NOT_FOUND));
 
     }
 
     public Page<Bidding> findAllByProductId(long productId, Pageable pageable) {
         Product product = productRepository.findById(productId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 ID의 product가 없습니다."));
+                .orElseThrow(() -> new BusinessException(NOT_FOUND));
 
         return biddingRepository.findAllByProduct(pageable, product);
     }
@@ -129,10 +140,5 @@ public class BiddingService {
                 }
             }
         }
-    }
-
-    private Member findMemberWithPessimisticLock(Member member) {
-        return memberRepository.findByIdWithPessimisticLock(member.getId())
-                .orElseThrow(() -> new BusinessException(MEMBER_NOT_FOUND));
     }
 }
