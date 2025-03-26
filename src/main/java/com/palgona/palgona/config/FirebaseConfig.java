f8a311e19248a41a81bfb9c5d869afcb906159e3
문진수 <mjinsu098@gmail.com>
2025-03-26 14:11:04 +0900

    [chore] : cd 인프라 설정 추가

--- src/main/java/com/palgona/palgona/config/FirebaseConfig.java
+++ src/main/java/com/palgona/palgona/config/FirebaseConfig.java
@@ -8,20 +8,20 @@ import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
 
-@Configuration
-public class FirebaseConfig {
-
-    @PostConstruct
-    public void init(){
-        try{
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/FCMAccountKey.json");
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-            FirebaseApp.initializeApp(options);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-}
+//@Configuration
+//public class FirebaseConfig {
+//
+//    @PostConstruct
+//    public void init(){
+//        try{
+//            FileInputStream serviceAccount =
+//                    new FileInputStream("src/main/resources/FCMAccountKey.json");
+//            FirebaseOptions options = new FirebaseOptions.Builder()
+//                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//                    .build();
+//            FirebaseApp.initializeApp(options);
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//    }
+//}
--- src/main/java/com/palgona/palgona/fcm/presentation/FCMController.java
+++ src/main/java/com/palgona/palgona/fcm/presentation/FCMController.java
@@ -24,7 +24,7 @@ public class FCMController {
     public ResponseEntity<Void> updateFCMToken(
             @AuthenticationPrincipal CustomMemberDetails member,
             @RequestBody FCMTokenUpdateRequest request
-            ){
+    ){
 
         fcmService.updateFCMToken(member, request);
 
