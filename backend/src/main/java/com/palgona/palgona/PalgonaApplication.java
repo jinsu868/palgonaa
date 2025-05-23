6d56105e448142bb460d8b5d93b2b83f0264665e
jinsu868 <mjinsu098@gmail.com>
2025-05-23 15:20:48 +0900

    chore : seperate schedule, jpa config from main application

--- backend/src/main/java/com/palgona/palgona/PalgonaApplication.java
+++ backend/src/main/java/com/palgona/palgona/PalgonaApplication.java
@@ -2,12 +2,8 @@ package com.palgona.palgona;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
-@EnableJpaAuditing
 public class PalgonaApplication {
 
 	public static void main(String[] args) {
